eb5d2e7f646a56f058bf2eb73f5a516989b9dea9
Patrick Seitz <seitzpr@gmail.com>
2024-08-10 01:10:33 -0400

    Added before/after FFT graph generation functions

--- plot/src/graph.py
+++ plot/src/graph.py
@@ -1,25 +1,88 @@
-from pathlib import Path
 import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.style as mplstyle
+import matplotlib.style
 import numpy as np
-import typing
-import types
+import pandas as pd
 import os
 
-class Graph:   
-    log_path: str = os.path.join(os.getcwd(),"..","..","documentation/output/plots/before_fft_11025_real_signal.txt")
-    
+
+class Graph:
+    before_path: str = os.path.join(
+        os.getcwd(),
+        "..",
+        "..",
+        "documentation/output/plots/before_fft_11025_real_signal.txt",
+    )
+    fft_path: str = os.path.join(
+        os.getcwd(),
+        "..",
+        "..",
+        "documentation/output/plots/after_fft_11025.txt",
+    )
+    fft_negative_path: str = os.path.join(
+        os.getcwd(),
+        "..",
+        "..",
+        "documentation/output/plots/after_fft_11025_negative.txt",
+    )
+    result_path: str = os.path.join(
+        os.getcwd(),
+        "..",
+        "..",
+        "documentation/output/plots/after_fft_11025_real_signal.txt",
+    )
+
     def __init__(self):
         self
 
-
     def open_file(self):
         print(self.log_path)
-        with open(self.log_path,"r") as file:
-            count:int = 0
+        with open(self.log_path, "r") as file:
+            count: int = 0
             for i in file:
                 line = i.strip()
                 print(f"{count}: {line}")
-                count+=1
-    
-    def load_into_df(self):
-        print("hello")
+                count += 1
+
+    def load_into_df(self, path: str):
+        df = pd.read_csv(path, dtype={"Real": float, "Imaginary": float})
+        row_count: int = len(df.index)
+        print(f"row count: {row_count}")
+        print(df)
+        return df
+
+    def generate_before_graph(self, df):
+        fig, ax = plt.subplots()
+        ax.plot(df["Real"])
+        ax.set(xlabel="Sample", ylabel="Frequency")
+        ax.grid()
+        fig.savefig("base.png")
+        plt.show()
+
+    # In the frequency domain
+    def generate_after_fft_graph(self, df):
+        fig, ax = plt.subplots()
+        ax.plot(df["Real"])
+        ax.set(xlabel="Frequency Bin", ylabel="Amount")
+        ax.grid()
+        fig.savefig("fft.png")
+        plt.show()
+
+    # In the frequency domain
+    def generate_after_fft_negative_graph(self, df):
+        fig, ax = plt.subplots()
+        ax.plot(df["Real"])
+        ax.set(xlabel="Frequency Bin", ylabel="Amount")
+        ax.grid()
+        fig.savefig("fft_negative.png")
+        plt.show()
+
+    def generate_result_graph(self, df):
+        fig, ax = plt.subplots()
+        ax.plot(df["Real"])
+        ax.plot(df[" Imaginary"])
+        ax.set(xlabel="Sample", ylabel="Frequency")
+        ax.grid()
+        fig.savefig("result.png")
+        plt.show()
--- plot/src/main.py
+++ plot/src/main.py
@@ -1,8 +1,48 @@
-from graph import Graph 
+from graph import Graph
+import os
+
 
 def main():
-    grapher = Graph()
-    grapher.open_file()
+    # File paths
+    before_path: str = os.path.join(
+        os.getcwd(),
+        "..",
+        "..",
+        "documentation/output/plots/before_fft_11025_real_signal.txt",
+    )
+    fft_path: str = os.path.join(
+        os.getcwd(),
+        "..",
+        "..",
+        "documentation/output/plots/after_fft_11025.txt",
+    )
+    fft_negative_path: str = os.path.join(
+        os.getcwd(),
+        "..",
+        "..",
+        "documentation/output/plots/after_fft_11025_negative.txt",
+    )
+    result_path: str = os.path.join(
+        os.getcwd(),
+        "..",
+        "..",
+        "documentation/output/plots/after_fft_11025_real_signal.txt",
+    )
+
+    graph = Graph()
+
+    # Load csv data into data frames
+    before = graph.load_into_df(before_path)
+    after_fft = graph.load_into_df(fft_path)
+    after_fft_negative = graph.load_into_df(fft_negative_path)
+    result = graph.load_into_df(result_path)
+
+    # Generate graphs
+    graph.generate_before_graph(before)
+    graph.generate_after_fft_graph(after_fft)
+    graph.generate_after_fft_negative_graph(after_fft_negative)
+    graph.generate_result_graph(result)
+
 
 if __name__ == "__main__":
     main()
