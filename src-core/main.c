72575d5b1ddd13af466873073817d6016e7b76fc
Patrick Seitz <seitzpr@gmail.com>
2024-10-17 01:59:41 -0400

    Implementing command line interface

--- src-core/main.c
+++ src-core/main.c
@@ -17,14 +17,48 @@
 */
 
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <sndfile.h>
 #include "apt.h"
 #include "image.h"
 
+void menu_help();
+void command_verbose();
+
 int main(int argc, char *argv[])
 {
+    if (argc >= 2)
+    {
+        // Check for help and ignore all other commands
+        for (int i = 1; i < argc; i++)
+        {
+            if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0)
+            {
+                menu_help();
+                return 1;
+            }
+        }
+
+        // Process commands
+        for (int i = 1; i < argc; i++)
+        {
+            if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--verbose") == 0)
+            {
+                command_verbose();
+            }
+            else
+            {
+                printf("Unknown command: %s\n", argv[i]);
+            }
+        }
+    }
+    else
+    {
+        printf("No commands given\n");
+    }
+
     // Create demodulated audio file
     // create_audio();
 
@@ -32,7 +66,17 @@ int main(int argc, char *argv[])
     // create_image(5512);
 
     // create_image_reverse("./documentation/samples/audio/20210720111842.wav", "output/images/apt_image_reverse_1.bmp", 5512);
-    create_image_reverse("documentation/samples/audio/NOAA1920190808-070600.wav", "output/images/apt_image_reverse_2.bmp", 5512);
+    // create_image_reverse("documentation/samples/audio/NOAA1920190808-070600.wav", "output/images/apt_image_reverse_2.bmp", 5512);
 
     return 0;
+}
+
+void menu_help()
+{
+    printf("*** Help Menu ***\n");
+}
+
+void command_verbose()
+{
+    printf("verbose=1\n");
 }
