9ab96656f918ff6752b2029353ab42b4deb841b5
Pat8901 <seitzpr@gmail.com>
2025-11-21 19:36:03 -0500

    Updated License

--- src-plot/main.py
+++ src-plot/main.py
@@ -1,13 +1,13 @@
 """
-    NOAA-APT - Lightweight APT decoder for NOAA weather satellites
-    Copyright (C) 2024 Patrick O'Brien-Seitz
+    AptDemod - Lightweight APT decoder for NOAA weather satellites
+    Copyright (C) 2025 pat8901
 
-    NOAA-APT is free software: you can redistribute it and/or modify
+    AptDemod is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    NOAA-APT is distributed in the hope that it will be useful,
+    AptDemod is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
